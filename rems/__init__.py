9e55602e1993e7e2aafab0d2e4630412fd9f1846
landwehrrl <landwehrrl@yahoo.com>
2015-07-08 09:15:56 +0200

    extracted callSQLStored procedure from validateUser

--- rems/__init__.py
+++ rems/__init__.py
@@ -8,75 +8,60 @@ app.secret_key = 'CHANGE_FOR_REAL_WORLD'
 
 mysql = MySQL()
 # MySQL configurations
-try:
-  app.config['MYSQL_DATABASE_USER'] = 'root'
-  app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-  app.config['MYSQL_DATABASE_DB'] = 'rems'
-  app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-  mysql.init_app(app)
-except Exception, e:
-  print e
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'rems'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
+def callSQLStoredProcedure(sp,*kwargs):
+  global conn,cursor
+  conn = mysql.connect()
+  cursor = conn.cursor()
+  cursor.callproc(sp,*kwargs)
+  return cursor.fetchall()
+  
 @app.route("/")
 def main():
   return render_template('index.html')
 
 @app.route('/rems')
 def rems():
-  return render_template('rems.html' , user = currentUser, session = session)
+  return render_template('rems.html' , user = data[0][1], session = session)
 
 @app.route('/validateLogin',methods=['POST'])
 def validateLogin():
-  try:
-    _name = request.form['inputName']
-    _password = request.form['inputPassword']
-    _hashed_password = generate_password_hash(_password)
+  _name = request.form['inputName']
+  _password = request.form['inputPassword']
     
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.callproc('sp_validateUser',(_name,))
-    data = cursor.fetchall()
-  
-    if len(data) > 0:
-      if check_password_hash(str(data[0][2]),_password):
-        session['user'] = data[0][0]
-        global currentUser
-        currentUser = str(data[0][1])
-        print 'currentUser ' + str(currentUser)
-        return redirect('/rems')
-      else:
-        return render_template('index.html', error = 'Incorrect Username or Password');
-    else:
-      return render_template('index.html', error = 'Incorrect Username or Password');
+  try:
+    data = callSQLStoredProcedure('sp_validateUser',(_name,))
+    if len(data) > 0 and check_password_hash(str(data[0][2]),_password):
+      session['user'] = data[0][0]
+      return render_template('rems.html' , user = data[0][1], session = session)
+    return render_template('index.html', error = 'Incorrect Username or Password');
   except Exception as e:
     print str(e)
     return render_template('index.html', error = 'General Error');
   finally:
     cursor.close()
     conn.close()
-
-    
+'''
+Only use to add users
+'''
 @app.route('/signUp',methods=['POST'])
 def signUp():
-  '''
-  Only used to add users use index.html.addusers
-  '''
-
   _name = request.form['inputName']
   _password = request.form['inputPassword']
   _hashed_password = generate_password_hash(_password)
-    
-  conn = mysql.connect()
-  cursor = conn.cursor()
-  cursor.callproc('sp_createUser',(_name,_hashed_password))
   
-  data = cursor.fetchall()
- 
+  data = callSQLStoredProcedure('sp_createUser',(_name,_hashed_password))
   if len(data) is 0:
     conn.commit()
-    return 'made it'
+    return 'Added User' #could move to render template if added as admin option later
   else:
-    return 'internal error'
+    return 'Failed' #could move to render template if admin option added later
+  #Maybe add roles for users, granting admin... or not.
   
 if __name__ == '__main__':
   app.run()
